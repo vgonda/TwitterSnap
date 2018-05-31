5e91ecad1cec5bf68d5611d46ef4dd832dbfdd49
Victoria Gonda <v.m.gonda@gmail.com>
2018-05-31 17:10:32 -0500

    Fix styling

--- app/src/main/java/com/raywenderlich/android/twittersnap/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/twittersnap/MainActivity.kt
@@ -41,90 +41,87 @@ import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.fab
-import kotlinx.android.synthetic.main.activity_main.toolbar
-import kotlinx.android.synthetic.main.content_main.imageView
-import kotlinx.android.synthetic.main.content_main.overlay
-import kotlinx.android.synthetic.main.content_main.progressBar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), MainActivityPresenter.View {
 
-    private lateinit var presenter: MainActivityPresenter
+  private lateinit var presenter: MainActivityPresenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        this.presenter = MainActivityPresenter(this)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setSupportActionBar(toolbar)
+    this.presenter = MainActivityPresenter(this)
 
-        setUpNewImageListener()
+    setUpNewImageListener()
+  }
+
+  private fun setUpNewImageListener() {
+    fab.setOnClickListener {
+      val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+      startActivityForResult(intent, 1)
     }
+  }
 
-    private fun setUpNewImageListener() {
-        fab.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent, 1)
-        }
+  override fun onActivityResult(requestCode: Int, resultCode: Int, imageReturnedIntent: Intent) {
+    super.onActivityResult(requestCode, resultCode, imageReturnedIntent)
+    when (requestCode) {
+      1 -> if (resultCode == Activity.RESULT_OK) {
+        val selectedImageBitmap = resizeImage(imageReturnedIntent.data)
+        imageView.setImageBitmap(selectedImageBitmap)
+        setUpCloudSearch(selectedImageBitmap)
+        overlay.clear()
+        presenter.runTextRecognition(selectedImageBitmap!!)
+      }
     }
+  }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, imageReturnedIntent: Intent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent)
-        when (requestCode) {
-            1 -> if (resultCode == Activity.RESULT_OK) {
-                val selectedImageBitmap = resizeImage(imageReturnedIntent.data)
-                imageView.setImageBitmap(selectedImageBitmap)
-                setUpCloudSearch(selectedImageBitmap)
-                overlay.clear()
-                presenter.runTextRecognition(selectedImageBitmap!!)
-            }
-        }
+  private fun setUpCloudSearch(selectedImageBitmap: Bitmap?) {
+    fab.setImageResource(R.drawable.ic_cloud_black_24dp)
+    fab.setBackgroundColor(getColor(R.color.colorPrimaryDark))
+    fab.setOnClickListener {
+      overlay.clear()
+      presenter.runCloudTextRecognition(selectedImageBitmap!!)
     }
+  }
 
-    private fun setUpCloudSearch(selectedImageBitmap: Bitmap?) {
-        fab.setImageResource(R.drawable.ic_cloud_black_24dp)
-        fab.setBackgroundColor(getColor(R.color.colorPrimaryDark))
-        fab.setOnClickListener {
-            overlay.clear()
-            presenter.runCloudTextRecognition(selectedImageBitmap!!)
-        }
+  private fun resizeImage(selectedImage: Uri): Bitmap? {
+    return getBitmapFromUri(selectedImage)?.let {
+      val scaleFactor = Math.max(
+          it.width.toFloat() / imageView.width.toFloat(),
+          it.height.toFloat() / imageView.height.toFloat())
+
+      Bitmap.createScaledBitmap(it,
+          (it.width / scaleFactor).toInt(),
+          (it.height / scaleFactor).toInt(),
+          true)
     }
+  }
 
-    private fun resizeImage(selectedImage: Uri): Bitmap? {
-        return getBitmapFromUri(selectedImage)?.let {
-            val scaleFactor = Math.max(
-                    it.width.toFloat() / imageView.width.toFloat(),
-                    it.height.toFloat() / imageView.height.toFloat())
-
-            Bitmap.createScaledBitmap(it,
-                    (it.width / scaleFactor).toInt(),
-                    (it.height / scaleFactor).toInt(),
-                    true)
-        }
-    }
-
-    private fun getBitmapFromUri(filePath: Uri): Bitmap? {
-        return MediaStore.Images.Media.getBitmap(this.contentResolver, filePath)
-    }
+  private fun getBitmapFromUri(filePath: Uri): Bitmap? {
+    return MediaStore.Images.Media.getBitmap(this.contentResolver, filePath)
+  }
 
 
-    override fun showHandle(text: String, boundingBox: Rect?) {
-        overlay.addText(text, boundingBox)
-    }
+  override fun showHandle(text: String, boundingBox: Rect?) {
+    overlay.addText(text, boundingBox)
+  }
 
-    override fun showBox(boundingBox: Rect?) {
-        overlay.addBox(boundingBox)
-    }
+  override fun showBox(boundingBox: Rect?) {
+    overlay.addBox(boundingBox)
+  }
 
-    override fun showNoTextMessage() {
-        Toast.makeText(this, "No text detected", Toast.LENGTH_LONG).show()
-    }
+  override fun showNoTextMessage() {
+    Toast.makeText(this, "No text detected", Toast.LENGTH_LONG).show()
+  }
 
-    override fun showProgress() {
-        progressBar.visibility = View.VISIBLE
-    }
+  override fun showProgress() {
+    progressBar.visibility = View.VISIBLE
+  }
 
-    override fun hideProgress() {
-        progressBar.visibility = View.GONE
-    }
+  override fun hideProgress() {
+    progressBar.visibility = View.GONE
+  }
 }
 
